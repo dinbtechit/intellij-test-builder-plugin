8d8b44457a1fb90153c020fe57b778a86319d00f
Dinesh Srinivasan <dinbtechit@gmail.com>
2020-12-14 10:12:32 -0400

    Adding a module builder and module type.

--- src/main/kotlin/com/github/dinbtechit/intellijtestbuilderplugin/services/MyProjectService.kt
+++ src/main/kotlin/com/github/dinbtechit/intellijtestbuilderplugin/services/MyProjectService.kt
@@ -1,7 +1,7 @@
 package com.github.dinbtechit.intellijtestbuilderplugin.services
 
-import com.intellij.openapi.project.Project
 import com.github.dinbtechit.intellijtestbuilderplugin.MyBundle
+import com.intellij.openapi.project.Project
 
 class MyProjectService(project: Project) {
 
